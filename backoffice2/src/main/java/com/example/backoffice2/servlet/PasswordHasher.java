3184d78e0a0c0c9bf024084975387cdde3eef234
Sylvia B <sylwiab@gmail.com>
2023-09-15 23:37:17 +0200

    Ajout PasswordHasher - à voir plus tard.

---
+++ backoffice2/src/main/java/com/example/backoffice2/servlet/PasswordHasher.java
@@ -0,0 +1,25 @@
+package com.example.backoffice2.servlet;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PasswordHasher {
+    public static String hashPassword(String password) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hashedPassword = md.digest(password.getBytes());
+        // Convert the byte array to a hexadecimal string
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hashedPassword) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
+    public static String maskPassword(String password) {
+        // Masquer le mot de passe comme vous le souhaitez, par exemple : "********"
+        return "********";
+    }
+}
